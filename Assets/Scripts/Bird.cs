fe2d418a9de35db2c6a8f0831ef70d5568ec11be
ercegang <ercegang@gmail.com>
2023-10-15 11:15:49 +0300

    End Screen , static speed

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -2,37 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bird : MonoBehaviour
 {
     public float jumpSpeed = 5;
-
     Rigidbody2D rb;
-
     public int score;
-
+    public TMP_Text scoreText;
+    public float speed;
+    public GameObject endScreen;
     private void Start()
+
+
+
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
+
+
+
 
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButton(0) && speed > 0)
         {
            rb.velocity = Vector2.up * jumpSpeed; 
         }
+
+        transform.eulerAngles = new Vector3(0, 0, rb.velocity.y * 7f);
+
+        Pipe.speed = speed;
     }
+
+
+
+
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        var currentScene = SceneManager.GetActiveScene().name;
-        SceneManager.LoadScene(currentScene);
+        Die();
     }
+
+    
+
+    public void Die()
+    {
+        speed = 0;
+        Invoke("ShowMenu",1f);
+
+        //var currentScene = SceneManager.GetActiveScene().name;
+        //SceneManager.LoadScene(currentScene);
+    }
+
+    void ShowMenu()
+    {
+        scoreText.text = "";
+        endScreen.SetActive(true);
+        print("end screen");
+
+    }
+
+
+
+
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         score++;
+
+        scoreText.text = score.ToString();
         
     }
     
